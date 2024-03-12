d1d54d7f1137ad58c2b204f55725b904164464d8
zorojurio <zorojurio@gmail.com>
2024-03-12 18:27:21 +0530

    add unit tests for signup

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 import pytest
 
+from accounts.models import CustomUser
 from accounts.tests.factories import UserFactory
 from django.contrib import auth
 
@@ -101,3 +102,61 @@ class TestCustomLogoutView:
         assert response.url == reverse("accounts:login")
         user = auth.get_user(authenticated_signer_client)
         assert user.is_authenticated is False
+
+
+class TestSignUpView:
+    """Test the SignUpView."""
+
+    signup_url = reverse("accounts:signup")
+
+    def test_get_signup_page(self, client):
+        """Test whether the signup page is rendered correctly."""
+        response = client.get(self.signup_url)
+        assert response.status_code == 200
+        assert response.template_name == ["accounts/signup.html"]
+        # check the form fields are included in the context form
+        assert response.context["form"].fields.keys() == {
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password1",
+            "password2",
+            "user_type",
+        }
+        # check the user_type field choices, should be signer and validator
+        assert response.context["form"].fields["user_type"].choices == [
+            ("signer", "Signer"),
+            ("validator", "Validator"),
+        ]
+
+    @pytest.mark.parametrize("user_type", ["signer", "validator"])
+    @pytest.mark.django_db
+    def test_signup(self, client, user_type):
+        """Test signup as a signer user and a validator user."""
+        username = "testuser-signer"
+        response = client.post(
+            self.signup_url,
+            data={
+                "username": username,
+                "first_name": "Test",
+                "last_name": "User",
+                "email": "test-signer@gmail.com",
+                "password1": "NewPassword@123",
+                "password2": "NewPassword@123",
+                "user_type": user_type,
+            },
+        )
+        assert response.status_code == 302
+        created_user: CustomUser = CustomUser.objects.filter(username=username).first()
+        assert created_user is not None
+        assert created_user.username == username
+        assert created_user.email == created_user.email
+        # newly created user should not be active
+        assert created_user.is_active is False
+        # newly created user should be a signer
+        assert created_user.user_type == getattr(CustomUser.UserType, user_type)
+        # check the success url for the redirection
+        assert response.url == reverse("accounts:login")
+        # user should not be authenticated
+        assert response.wsgi_request.user.is_authenticated is False
