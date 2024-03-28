befe1460826f0e33cb3cf64b776bb824fe03adcc
zorojurio <zorojurio@gmail.com>
2024-03-29 00:02:46 +0530

    add postgres as the DB

--- validator/settings.py
+++ validator/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = "validator.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "pdf_validator",
+        "USER": "user",
+        "PASSWORD": "localdev",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
