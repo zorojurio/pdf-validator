527526c4454283b0874631e380217d714fa2073b
zorojurio <zorojurio@gmail.com>
2024-03-29 22:29:25 +0530

    add MIS reports

--- signature_validator/views.py
+++ signature_validator/views.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+from datetime import timedelta
+
+from django.utils import timezone
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import QuerySet
 from django.views.generic import CreateView, DetailView, ListView, TemplateView
 
+from accounts.models import CustomUser, SignerUser
 from .forms import PdfValidateForm
 from .models import PdfDocumentValidator
 
@@ -67,4 +72,48 @@ class ReportView(TemplateView):
         context["verified_pdf"] = validated_pdf.filter(
             all_signers_verified=True
         ).count()
+        context["pdf_not_verified"] = validated_pdf.filter(
+            all_signers_verified=False
+        ).count()
+        context["users_count"] = CustomUser.objects.all().count()
+        context["signers_count"] = SignerUser.objects.all().count()
+        context["today_date"] = timezone.now().date()
+        signed_documents_data = []
+        days = []
+
+        for i in range(1, 6):
+            current_day = timezone.now() - timedelta(days=i)
+            start_of_day = current_day.replace(
+                hour=0, minute=0, second=0, microsecond=0
+            )
+            end_of_day = current_day.replace(
+                hour=23, minute=59, second=59, microsecond=999999
+            )
+            signed_documents_count = PdfDocumentValidator.objects.filter(
+                is_signed=True, created__range=(start_of_day, end_of_day)
+            ).count()
+            signed_documents_data.append(signed_documents_count)
+            days.append(str(current_day.date()))
+        context["signed_documents_labels"] = days
+        context["signed_documents_data"] = signed_documents_data
+
+        users_data = []
+        users_label = []
+        for i in range(1, 7):
+            current_day = timezone.now() - timedelta(days=i)
+            start_of_day = current_day.replace(
+                hour=0, minute=0, second=0, microsecond=0
+            )
+            end_of_day = current_day.replace(
+                hour=23, minute=59, second=59, microsecond=999999
+            )
+            users_joined_count = CustomUser.objects.filter(
+                is_active=True, date_joined__range=(start_of_day, end_of_day)
+            ).count()
+            users_data.append(users_joined_count)
+            users_label.append(str(current_day.date()))
+        context["signed_documents_labels"] = days
+        context["signed_documents_data"] = signed_documents_data
+        context["users_data"] = users_data
+        context["users_label"] = users_label
         return context
